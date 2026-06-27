645c31f5dc79f0061de20b2d479ad501ecf76f9e
Tanvir Ahmed <mta.coder97@gmail.com>
2026-06-27 22:43:42 +0000

    chore: Renamed theme toggle component name and filename. Also removed site header actions extension slot.

--- apps/web/src/components/layout/site-header.tsx
+++ apps/web/src/components/layout/site-header.tsx
@@ -1,10 +1,8 @@
 import { UserRound } from "lucide-react";
 
-import { ExtensionSlot } from "@repo/extension-api";
-
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import UserDropdown from "./user-dropdown";
-import { ModeToggle } from "./mode-toggle";
+import { ThemeToggle } from "./theme-toggle";
 import type { AppUser } from "@/models/auth";
 import { Button } from "../ui/button";
 
@@ -19,8 +17,7 @@ export function SiteHeader({ user }: Props) {
 				<SidebarTrigger className="-ml-1 h-8 w-8 cursor-pointer" />
 			</div>
 			<div className="flex items-center gap-1">
-				<ExtensionSlot name="site-header-actions" />
-				<ModeToggle />
+				<ThemeToggle />
 				<UserDropdown
 					user={user}
 					defaultOpen={false}
